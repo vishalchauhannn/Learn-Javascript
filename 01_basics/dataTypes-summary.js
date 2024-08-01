c01bd1b36820ce234c5f4cf8afc3316cf9354593
vishalchauhannn <vishal626chauhan@gmail.com>
2024-08-01 14:05:49 +0530

    dataType-summary completed

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -26,9 +26,34 @@ let myObj = {
 }
 
 const myFunction = function(){
-console.log("Hello World");
+// console.log("Hello World");
 }
 
-console.log(typeof(myArray));   // object
-console.log(typeof(myObj));     // object
-console.log(typeof(myFunction)); // object Function
+// console.log(typeof(myArray));   // object
+// console.log(typeof(myObj));     // object
+// console.log(typeof(myFunction)); // object Function
+
+
+// ---------------------------- MEMORY (stack & heap) -----------------------------------
+
+// Stack memory (Primitive dataTypes) it gives us a copy
+let name = "Vishal";
+let anotherName = name;
+anotherName = "aman"
+// console.log(anotherName)
+// console.log(name);
+
+// // Heap memory (reference & non-primitive)
+
+let userOne = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+// console.log(userTwo);
+
+userTwo.email = "vishal@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
